45a562f310ede666dc9120d36c5ef032267094ec
gmsaldanha <gmscomputadores@bol.com.br>
2021-07-16 18:20:31 -0300

    Update edittransportador.blade.php

--- resources/views/fretes/edittransportador.blade.php
+++ resources/views/fretes/edittransportador.blade.php
@@ -6,98 +6,78 @@
     
 <div class="card border">
   <div class="card-body">
-  <form action="{{ route('atucontas', ['id' => $contas->id]) }}" method="post">
+  <form action="{{ route('atutransportadora', ['id' => $transportadors->id]) }}" method="post">
 @method('put')
 @csrf
 
-      <div class="form-row">
-        <div class="form-group col-md-5">
-          <label for="nomebanco">Nome do Banco</label>
-          <input type="text" class="form-control" name="nomebanco" id="nomebanco" value="{{ $contas->Banco }}">
-        </div>
-
-        <div class="form-group col-md-2">
-          <label for="numbanco">Numero</label>
-          
-          <input type="text" class="form-control" name="numbanco" id="numbanco" value="{{ $contas->NumBanco }}">
-        </div>
-        
-
-        <div class="form-group col-md-2">
-          <label for="agencia">Agencia</label>
-          
-          <input type="text" class="form-control" name="agencia" id="agencia" value="{{ $contas->Agencia }}">
-        </div>
-
-        <div class="form-group col-md4">
-          <label for="padrao">Definir como Padrao</label>
-          <input type="checkbox" class="form-control"  name="padrao" id="padrao"value="{{ $contas->Padrao}}">
-
-        </div>
-
-      </div>
-
-      <div class="form-row">
-        <div class="form-group col-md-3">
-          <label for="conta">Conta</label>
-          <input type="text" class="form-control" name="conta" id="conta" value="{{ $contas->Conta }}">
-        </div>
-
+<div class="form-row">
         <div class="form-group col-md-4">
-          <label for="pix">Pix</label>
-          
-          <input type="text" class="form-control" name="pix" id="pix" value="{{ $contas->CodigoPix }}">
-        </div>
-
-        <div class="form-group col-md-5">
-          <label for="titular">Titular</label>
-          
-          <input type="text" class="form-control" name="titular" id="titular" value="{{ $contas->Titular }}">
-        </div>
-
+          <label for="Transportadora">Transportadora</label>
+          <input type="text" class="form-control" name="Transportadora" id="Transportadora" value="{{ $transportadors->Transportadora }}">
       </div>
-
-      <div class="form-row">
-        <div class="form-group col-md-6">
-          <label for="logradouro">Logradouro</label>
-          <input type="text" class="form-control" name="logradouro" id="logradouro" value="{{ $contas->Logradouro }}">
+        <div class="form-group col-md-8">
+          <label for="Url_transp">Url_transp</label>
+          <input type="text" class="form-control" name="Url_transp" id="Url_transp" value="{{ $transportadors->Url_transp }}">
+        </div>
         </div>
 
-        <div class="form-group col-md-5">
-          <label for="cidade">Cidade</label>
-          
-          <input type="text" class="form-control" name="municipio" id="municipio" value="{{ $contas->Municipio }}">
+        <div class="form-row">
+        <div class="form-group col-md-12">
+          <label for="Token">Token</label>          
+          <input type="text" class="form-control" name="Token" id="Token" value="{{ $transportadors->Token }}">
         </div>
-        <div class="form-group col-md-1">
-          <label for="uf">Estado</label>
-          
-          <input type="text" class="form-control" name="uf" id="uf" value="{{ $contas->Uf }}">
-        </div>
-
-
-
-
 
 
         <div class="form-row">
-        <div class="form-group col-md-3">
-          <label for="txid">TxId</label>
-          
-          <input type="text" class="form-control" name="txid" id="txid" value="{{ $contas->TxId }}">
+        <div class="form-group col-md-6">
+          <label for="Url_Redir">Url_Redir</label>          
+          <input type="text" class="form-control" name="Url_Redir" id="Url_Redir" value="{{ $transportadors->Url_Redir }}">
         </div>
 
-        <div class="form-group col-md-5">
-          <label for="data">Data de Cadastro</label>
-          
-          <input type="date" class="form-control" name="data" id="data"value="{{ $contas->Data }}">
+        <div class="form-group col-md-6">
+          <label for="CallBack">CallBack</label>          
+          <input type="text" class="form-control" name="CallBack" id="CallBack" value="{{ $transportadors->CallBack }}">
         </div>
 
 
+
+      <div class="form-row">
+        <div class="form-group col-md-12">
+          <label for="Client_id">Client_id</label>          
+          <input type="text" class="form-control" name="Client_id" id="Client_id" value="{{ $transportadors->Client_id }}">
+        </div>
+
+
+      <div class="form-row">
+        <div class="form-group col-md-12">
+          <label for="Authorization">Authorization</label>
+          <input type="text" class="form-control" name="Authorization" id="Authorization" value="{{ $transportadors->Authorization }}">
+        </div>
 
 
         
-      </div>
+      
 
+
+        <div class="form-group col-md-5">
+          <label for="Content_Type">Content_Type</label>          
+          <input type="text" class="form-control" name="Content_Type" id="Content_Type" value="{{ $transportadors->Content_Type }}">
+        </div>
+        <div class="form-group col-md-4">
+          <label for="scope">Scope</label>          
+          <input type="text" class="form-control" name="scope" id="scope" value="{{ $transportadors->scope }}">
+        </div>
+
+
+
+  
+
+  </div>
+
+  <div class="form-row">
+  <div classs="box"> 
+  </div>    
+  <div classs="box"> 
   </div>
 
   <div class="form-row">
@@ -120,9 +100,7 @@
          
     <div classs="box"> 
      <button type="submit" class="btn btn btn-primary" >Salvar</button>
-     <a href="contas" class="btn btn btn-danger" >Cancelar</a>
-   
-    <a href="{{route('psps' )}}" class="btn btn-primary">Cadastrar PSP</a>
+     <a href="transportadoras" class="btn btn btn-danger" >Cancelar</a>
 
    
     </div>  
